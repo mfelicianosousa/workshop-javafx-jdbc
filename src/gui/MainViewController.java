51438d8b50ac9a19cbc29e98ab12bff9720d4d9c
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2024-05-20 17:53:14 -0400

    DepartmentList controller

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -34,7 +34,7 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onMnuItemDepartmentAction() {
-		loadView("/gui/DepartamentList.fxml");
+		loadView("/gui/DepartmentList.fxml");
 	}
 	
 	@FXML
