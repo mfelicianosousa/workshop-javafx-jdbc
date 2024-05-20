b88038974d72c7b683cc131b035c9f6ac7e12a0b
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2024-05-20 15:10:17 -0400

    Main view

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,12 +1,14 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+	/*
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -19,6 +21,23 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
+	*/
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+			Parent parent = loader.load();
+			
+			Scene mainScene = new Scene(parent);
+			primaryStage.setScene(mainScene);
+			primaryStage.setTitle("Sample JavaFX application");
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 	
 	public static void main(String[] args) {
 		launch(args);
